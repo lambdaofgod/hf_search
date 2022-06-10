dd0ec063af03b83e51fb528b2cbfffd078ac8d5c
lambdaofgod <bartczukkuba@gmail.com>
2022-06-10 21:38:49 +0200

    package cleanup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='hf_search',
+    packages=find_packages(),
+    version='0.1.0',
+    description='better search for huggingface datasets',
+    author='Jakub Bartczuk',
+    license='MIT',
+)
